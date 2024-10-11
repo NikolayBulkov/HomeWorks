74a8e90ce6bfcbb9f5350414144e3457eb9a2a31
Булков Николай <sgmkhp@yandex.ru>
2024-10-11 23:45:15 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+count_ht = 12
+count_time = 1.5
+name_course = "Python"
+time_for_one_task = count_time / count_ht
+print ("Курс:", name_course + "," , " всего задач:" , count_ht , "," , " затрачено часов:" , count_time, "," , " среднее время выполнения:" , time_for_one_task, " часа.", sep="")
